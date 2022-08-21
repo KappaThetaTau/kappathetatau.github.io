ad2842061becc73eec253b176d2a37565573a714
atharvanaik <atharvanaik10@gmail.com>
2022-08-21 10:36:11 -0500

    Update sp22 pc in brothers page

--- scripts/update_data.py
+++ scripts/update_data.py
@@ -26,6 +26,7 @@ ALUMNI_IDX = 8
 
 data = {
     'actives': [
+        {'semester': 'Spring 2022', 'members': []},
         {'semester': 'Fall 2021', 'members': []},
         {'semester': 'Spring 2021', 'members': []},
         {'semester': 'Fall 2020', 'members': []},
